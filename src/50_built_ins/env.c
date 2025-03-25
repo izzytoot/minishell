08c88a8ca6272289e4d9408077b656e60af869c4
Daniela Sofia Do Carmo Padilha <ddo-carm@c1r11s4.42porto.com>
2025-03-25 19:04:38 +0000

    fixed env seg fault, missing newline

--- src/50_built_ins/env.c
+++ src/50_built_ins/env.c
@@ -6,7 +6,7 @@
 /*   By: ddo-carm <ddo-carm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 17:38:21 by ddo-carm          #+#    #+#             */
-/*   Updated: 2025/03/25 18:44:30 by ddo-carm         ###   ########.fr       */
+/*   Updated: 2025/03/25 19:04:05 by ddo-carm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,18 @@
 int	print_env(t_minishell **msh)
 {
 	int	i;
+	t_list *temp;
 	
 	// if ((*msh)->token_list->content && (*msh)->token_list->next)
 	// 	return (ft_putstr_fd(ERR_MANY_ARGS, STDERR_FILENO), 1);
 	i = 0;
-	if (!(*msh)->envp)
+	if (!(*msh)->envp_list)
 		return (EXIT_FAILURE);
-	while(&(*msh)->envp[i])
+	temp = (*msh)->envp_list;
+	while(temp)
 	{
-		ft_putstr_fd((*msh)->envp[i], STDOUT_FILENO);
-		i++;
+		ft_putstr_fd(temp->content, STDOUT_FILENO);
+		temp = temp->next;
 	}
 	return(EXIT_SUCCESS);
 }
