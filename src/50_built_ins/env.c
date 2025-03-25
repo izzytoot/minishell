b25f923e8fe0e0a3b377437949e27e6fca4db29b
Daniela Sofia Do Carmo Padilha <ddo-carm@c1r11s4.42porto.com>
2025-03-25 18:53:21 +0000

    still seg fault but better

--- src/50_built_ins/env.c
+++ src/50_built_ins/env.c
@@ -6,7 +6,7 @@
 /*   By: ddo-carm <ddo-carm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 17:38:21 by ddo-carm          #+#    #+#             */
-/*   Updated: 2025/03/25 18:19:59 by ddo-carm         ###   ########.fr       */
+/*   Updated: 2025/03/25 18:44:30 by ddo-carm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int	print_env(t_minishell **msh)
 {
 	int	i;
 	
-	if ((*msh)->token_list->content && (*msh)->token_list->next)
-		return (ft_putstr_fd(ERR_MANY_ARGS, STDERR_FILENO), 1);
+	// if ((*msh)->token_list->content && (*msh)->token_list->next)
+	// 	return (ft_putstr_fd(ERR_MANY_ARGS, STDERR_FILENO), 1);
 	i = 0;
 	if (!(*msh)->envp)
 		return (EXIT_FAILURE);
