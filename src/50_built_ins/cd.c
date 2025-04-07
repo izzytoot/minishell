67e4cee893a32eaecd052e0e5a6cfff2e5a30494
Daniela-Padilha <ddo-carm@LAPTOP-REDJ8VVL.>
2025-04-07 13:29:18 +0100

    started unset

--- src/50_built_ins/cd.c
+++ src/50_built_ins/cd.c
@@ -6,7 +6,7 @@
 /*   By: ddo-carm <ddo-carm@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 13:08:37 by ddo-carm          #+#    #+#             */
-/*   Updated: 2025/04/06 23:50:17 by ddo-carm         ###   ########.fr       */
+/*   Updated: 2025/04/07 11:26:42 by ddo-carm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,8 @@ int	ft_cd(t_minishell **msh, t_tree_node **node)
 		return (free(old_pwd), EXIT_FAILURE);
 	if (chdir(target_dir) == -1)
 	{
-		ft_printf("msh: cd: %s: No such file or directory\n", target_dir);
+		ft_dprintf(STDERR_FILENO, "msh: cd: %s: No such file or directory\n",
+			target_dir);
 		return (free(old_pwd), EXIT_FAILURE);
 	}
 	update_cd_env(msh, old_pwd);
---
+++ src/50_built_ins/unset.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unset.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddo-carm <ddo-carm@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/07 12:58:50 by ddo-carm          #+#    #+#             */
+/*   Updated: 2025/04/07 12:58:50 by ddo-carm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/minishell.h"
+
+//info -->
+
+int	ft_unset(t_minishell **msh, t_tree_node **node)
+{
+	int	i;
+
+	if (!node || !*node || !(*node)->args)
+		return (0);
+	i = 0;
+	while ((*node)->args[i])
+	{
+		ft_delete_var();
+	}
+	return (0);
+}
+
+//info --> delete a env var
