6c471ed83d86f0088ff653de479226536a607f85
Daniela-Padilha <ddo-carm@LAPTOP-REDJ8VVL.>
2025-04-30 17:58:59 +0100

    echo -n and '' handled

--- src/50_built_ins/53_echo.c
+++ src/50_built_ins/53_echo.c
@@ -6,55 +6,65 @@
 /*   By: ddo-carm <ddo-carm@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 18:10:15 by ddo-carm          #+#    #+#             */
-/*   Updated: 2025/04/29 17:11:42 by ddo-carm         ###   ########.fr       */
+/*   Updated: 2025/04/30 17:52:50 by ddo-carm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
 //info --> prints the array using binary tree
-int	handle_n(t_tree_nd **node);
+bool	handle_n(t_tree_nd **node, int *i);
 
 int	ft_echo(t_tree_nd **node)
 {
 	int		i;
-	t_quote	*quote;
-	bool	newline;
+	bool	nline;
 
+	i = 0;
 	if (!node || !*node)
 		return (EXIT_FAILURE);
-	newline = true;
-	i = handle_n(node);
-	if (i > 0)
-		newline = false;
-	quote = (*node)->quote_lst;
-	for (int j = 0; j < i && quote; j++)
-		quote = quote->next;
+	nline = handle_n(node, &i);
 	while ((*node)->args[i])
 	{
 		ft_putstr_fd((*node)->args[i], STDOUT_FILENO);
-		if ((*node)->args[i + 1] && quote && quote->next)
+		if ((*node)->args[i + 1] && (*node)->quote_lst && (*node)->quote_lst->next)
 		{
-			if (!(quote->in_quotes && quote->next->in_quotes))
+			if (!((*node)->quote_lst->in_quotes) && !((*node)->quote_lst->next->in_quotes))
 				ft_putstr_fd(" ", STDOUT_FILENO);
 		}
 		i++;
-		if (quote)
-			quote = quote->next;
+		if ((*node)->quote_lst)
+			(*node)->quote_lst = (*node)->quote_lst->next;
 	}
-	if (newline)
+	if (nline)
 		ft_putstr_fd("\n", STDOUT_FILENO);
 	return (0);
 }
 
-int	handle_n(t_tree_nd **node)
+bool	handle_n(t_tree_nd **node, int *i)
 {
-	int	i;
+	int		j;
+	bool	newline;
+	bool	first_flag;
 
-	i = 0;
-	while ((*node)->args[i] && ft_strncmp((*node)->args[i], "-n", 2) == 0)
+	first_flag = true;
+	while ((*node)->args[*i] && ft_strncmp((*node)->args[*i], "-n", 2) == 0)
 	{
-		i++;
+		j = 1;
+		newline = false;
+		while ((*node)->args[*i][j])
+		{
+			if ((*node)->args[*i][j] == 'n')
+				j++;
+			else if ((*node)->args[*i][j] != 'n')
+			{
+				if (first_flag)
+					newline = true;
+				return (newline);
+			}
+		}
+		first_flag = false;
+		(*i)++;
 	}
-	return (i);
+	return (newline);
 }
