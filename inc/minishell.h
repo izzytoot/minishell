2dc945a4d29f8b250e03638a3f03efd0bb8051d6
Daniela-Padilha <ddo-carm@LAPTOP-REDJ8VVL.>
2025-05-16 17:15:51 +0100

    added escape char handling in export, and removed trailing \n in unset

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ddo-carm <ddo-carm@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 12:50:18 by root              #+#    #+#             */
-/*   Updated: 2025/05/16 13:04:20 by ddo-carm         ###   ########.fr       */
+/*   Updated: 2025/05/16 16:59:13 by ddo-carm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -405,6 +405,7 @@ int				ft_export(t_msh **msh, t_tree_nd **node);
 t_list			*sort_env(t_list *env_list, int sort);
 void			disp_exported(t_msh **msh);
 t_list			*copy_env_list(t_list *env_list);
+char			*escape_value(char *value, int i, int j);
 
 //57_export_utils.c
 bool			is_valid_identifier(char *arg);
