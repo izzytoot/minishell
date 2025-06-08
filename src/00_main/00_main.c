8dcea70dbd433a36122825ff31b1baeddf286dec
isabeltootill <isabeltootill@gmail.com>
2025-06-08 22:26:25 +0100

    joinning with main

--- src/00_main/00_main.c
+++ src/00_main/00_main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   00_main.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: icunha-t <icunha-t@student.42.fr>          +#+  +:+       +#+        */
+/*   By: isabel <isabel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 14:28:40 by icunha-t          #+#    #+#             */
-/*   Updated: 2025/06/08 15:11:41 by icunha-t         ###   ########.fr       */
+/*   Updated: 2025/06/08 22:23:36 by isabel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	main(int ac, char **av, char **envp)
 	if (!msh)
 		close_minishell(msh, EXIT_FAILURE);
 	msh->active = true;
-	msh->debug_mode = true; //DELETE BEFORE SUMISSION
+	msh->debug_mode = false; //DELETE BEFORE SUMISSION
 	ft_init_msh(&msh, envp);
 	return (0);
 }
