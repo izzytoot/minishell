2585f6ad49231902648a61c82b936e528cda37a3
icunha-t <isabeltootill@gmail.com>
2025-06-09 14:26:38 +0100

    sigpipe resolved

--- src/70_exec/79_exec_cmd_utils.c
+++ src/70_exec/79_exec_cmd_utils.c
@@ -6,7 +6,7 @@
 /*   By: icunha-t <icunha-t@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 16:54:29 by ddo-carm          #+#    #+#             */
-/*   Updated: 2025/06/09 15:11:48 by icunha-t         ###   ########.fr       */
+/*   Updated: 2025/06/09 15:12:41 by icunha-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,9 @@ void	deal_with_cmd(t_msh **msh, t_tree_nd *node, int *status)
 	*status = exec_cmd(msh, node);
 }
 
-int	exec_cmd_child(t_msh **msh, t_tree_nd *node, int *status)
-{
-	signal(SIGQUIT, SIG_DFL);
-	signal(SIGINT, sig_c_child);
-	if (node->type == ENV_CMD || node->type == ARG)
-	{
-		if (node->cmd_content)
-			ft_free_arrays((void **)node->cmd_content);
-		node->cmd_content = join_cmd_and_args((node->cmd), node->args);
-		*status = exec_env_cmd(&(*msh), node);
-		return (exit_value(msh, *status, 1, 1));
-	}
-	//return (exit_value(msh, *status, 1, 1));
-	return (output_cmd_errors(msh, node));
-}
+// int	exec_cmd_child(t_msh **msh, t_tree_nd *node, int *status)
+// {
+// 	int	pid;
 
 // 	pid = safe_fork(msh);
 // 	if (pid == 0)
